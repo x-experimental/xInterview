9beb068a2a44a0fb76411c287c74c861e7a722de
xtyuns <xtyuns@163.com>
2023-08-09 14:05:06 +0800

    init project

---
+++ src/main/kotlin/io/github/xtyuns/xinterview/XInterviewApplication.kt
@@ -0,0 +1,11 @@
+package io.github.xtyuns.xinterview
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class XInterviewApplication
+
+fun main(args: Array<String>) {
+    runApplication<XInterviewApplication>(*args)
+}
---
+++ src/test/kotlin/io/github/xtyuns/xinterview/XInterviewApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.xtyuns.xinterview
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class XInterviewApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
